d3580adce4be564bb758de4d0da7ba726a2389d4
Nelson D. Sanchez E <nsanchezes@proveedor.intercorp.com.pe>
2021-10-04 22:22:34 -0500

    manejo de errores en el formulario register

--- src/components/auth/RegisterScreen.js
+++ src/components/auth/RegisterScreen.js
@@ -1,36 +1,79 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useForm } from '../../hooks/useForm';
+import validator from 'validator';
 
 export const RegisterScreen = () => {
+  const [formValues, handleInputChange] = useForm({
+    name: 'Nelson',
+    email: 'nelson@gmail.com',
+    password: '@1%$#df',
+    password2: '@1%$#df',
+  });
+
+  const { name, email, password, password2 } = formValues;
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (isFormValid()) {
+      console.log('formulario correcto');
+    }
+  };
+
+  const isFormValid = () => {
+    if (name.trim().length === 0) {
+      console.log('name is required');
+      return false;
+    } else if (!validator.isEmail(email)) {
+      console.log('email no válido');
+      return false;
+    } else if (password !== password2 || password.length < 5) {
+      console.log('password no válido');
+      return false;
+    }
+
+    return true;
+  };
+
   return (
     <>
       <h3 className='auth__title mb-5'>Register</h3>
-      <form>
+      <form onSubmit={handleSubmit}>
+        <div className='auth__alert-error'>Error de datos</div>
         <input
           className='auth__input'
           type='text'
           placeholder='Name'
           name='name'
           autoComplete='off'
+          value={name}
+          onChange={handleInputChange}
         />
         <input
           className='auth__input'
-          type='email'
+          type='text'
           placeholder='Email'
           name='email'
           autoComplete='off'
+          value={email}
+          onChange={handleInputChange}
         />
         <input
           className='auth__input'
           type='password'
           placeholder='Password'
           name='password'
+          value={password}
+          onChange={handleInputChange}
         />
         <input
           className='auth__input'
           type='password'
           placeholder='Confirm password'
-          name='confirm'
+          name='password2'
+          value={password2}
+          onChange={handleInputChange}
         />
         <button
           className='btn btn-primary btn-block mb-5'
