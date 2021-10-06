0c24bf2311577ebd7b99645817d0a1f34863189b
Nelson <nelson.se@globant.com>
2021-10-05 20:18:09 -0500

    mostrar un loading globant en la aplicacion

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import { onAuthStateChanged } from '@firebase/auth';
+import React, { useEffect, useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,8 +9,32 @@ import {
 
 import { JournalScreen } from '../components/journal/JournalScreen';
 import { AuthRouter } from './AuthRouter';
+import { auth } from '../firebase/firebaseConfig';
+import { useDispatch } from 'react-redux';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+  const dispatch = useDispatch();
+
+  const [checking, setChecking] = useState(true);
+
+  const [isLoggeIn, setIsLoggeIn] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user?.uid) {
+        dispatch(login(user.uid, user.displayName));
+        setIsLoggeIn(true);
+      } else {
+        setIsLoggeIn(false);
+      }
+      setChecking(false);
+    });
+  }, [dispatch, setChecking, setIsLoggeIn]);
+
+  if (checking) {
+    return <h1>ESPERE....</h1>;
+  }
   return (
     <Router>
       <div>
